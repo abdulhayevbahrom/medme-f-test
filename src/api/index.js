c0949c5f0c8961f2a4b94f67488993239ee7265e
abdulhayevbahrom <abdulhayevbahromjon2002@gmail.com>
2025-08-19 19:00:43 +0500

    last veriong

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const mainURL = axios.create({
   // baseURL: "http://localhost:8040/api",
-  baseURL: "https://medme-b-test.vercel.app/api",
+  baseURL: "http://167.86.96.232:8040/api",
 });
 
 export default mainURL;
--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 
 // const SOCKET_URL = `http://localhost:8040`;
-const SOCKET_URL = `https://medme-b-test.vercel.app/`;
+const SOCKET_URL = `http://167.86.96.232:8040/`;
 const headers = { transports: ["websocket"] };
 const socket = io(SOCKET_URL, headers);
 
